a4db0d194c3879219bd259a695c09c193cdacda5
Torstein Meland <torstein.meland@gmail.com>
2021-09-08 13:46:07 +0200

    Add IBOutlets for GUI elements

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currecyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
